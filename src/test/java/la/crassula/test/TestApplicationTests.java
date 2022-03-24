34fa0abdc56e5dac5d18c560b1b4e923434435cd
viktorija <viktorijamitrosenko@gmail.com>
2022-03-24 19:55:19 +0200

    ADD: project

---
+++ src/test/java/la/crassula/test/TestApplicationTests.java
@@ -0,0 +1,17 @@
+package la.crassula.test;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Duration;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
